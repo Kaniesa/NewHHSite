a14ea230ea38f74be41b28e3e26809671f8a513c
Naayeli007 <135177539+Naayeli007@users.noreply.github.com>
2024-04-28 11:34:24 -0400

    Create HighSchoolTeam.js
    Signed-off-by: Naayeli007 <135177539+Naayeli007@users.noreply.github.com>

---
+++ client/src/pages/HighSchoolTeam.js
@@ -0,0 +1,385 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Faq from '../components/Faq'
+
+import Paris from '../assets/img/team/Paris.webp'
+
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Team</h2>
+          <p>Our High School Team</p>
+        </div>
+
+        <div class="row" data-aos="fade-left">
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Anne"><img src="assets/img/team/anne.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Anne Zhang</h4>
+                <span>President</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="300">
+              <div class="pic" data-bioName="Grace"><img src="assets/img/team/Grace Qiu.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Grace Qiu</h4>
+                <span>Vice President</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Gurman"><img src="assets/img/team/Gurman.jpg" width="6000" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Gurman Sahota</h4>
+                <span>Vice President</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Kaniesa"><img src="assets/img/team/Kaniesa.png" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Kaniesa Deswal</h4>
+                <span>Tech Team Director</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+    
+         <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Jimmy"><img src="assets/img/team/Jimmy.jpeg" class="img-fluid tmpSize" alt=""></div>
+              <div class="member-info">
+                <h4>Jimmy Tao</h4>
+                <span>Students and Pairing Director</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>    
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Cici"><img src="assets/img/team/Cici.png" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Cici Zhu</h4>
+                <span>Volunteers Team Director</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Priyanshi"><img src="assets/img/team/Priyanshi.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Priyanshi Dokania</h4>
+                <span>QA Director</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <!--Non-Department Head Members-->
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Maggie"><img src="assets/img/team/Naayeli.png" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Naayeli Prakash</h4>
+                <span>Tech Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Maggie"><img src="assets/img/team/Mag.JPG" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Maggie Hu</h4>
+                <span>Pairing Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="LeahChin"><img src="assets/img/team/Leah C.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Leah Chin</h4>
+                <span>Pairing Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="FionaJ"><img src="assets/img/team/Fiona.jpeg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Fiona Jiang</h4>
+                <span>Pairing Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Hyewon"><img src="assets/img/team/Hyewon Image.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Hyewon Lee</h4>
+                <span>Media & Marketing Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Marwa"><img src="assets/img/team/Marwa Image.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Marwa Kabir</h4>
+                <span>Media & Marketing Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Chloe"><img src="assets/img/team/Chloe Image.JPG" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Chloe Cho</h4>
+                <span>Media & Marketing Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Chloe"><img src="assets/img/team/Nihitha image.JPG" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Nihitha Sriram</h4>
+                <span>Media & Marketing Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Cindy"><img src="assets/img/team/Cindy Image.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Cindy Li</h4>
+                <span>Media & Marketing Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Morgan"><img src="assets/img/team/Morgan.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Morgan Pankarican</h4>
+                <span>Media & Marketing Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Erin"><img src="assets/img/team/Erin.png" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Erin Yang</h4>
+                <span>Volunteers Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Zaynah"><img src="assets/img/team/Zaynah.png" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Zaynah Kashif</h4>
+                <span>Volunteers Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Ayushi"><img src="assets/img/team/Ayushi Pandit.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Ayushi Pandit</h4>
+                <span>QA Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Camille"><img src="assets/img/team/Camille Bernardo.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Camille Bernardo</h4>
+                <span>QA Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Lucy"><img src="assets/img/team/Lucy Chen.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Lucy Chen</h4>
+                <span>QA Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="Krisha Raichura"><img src="assets/img/team/Krisha Raichura.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Krisha Raichura</h4>
+                <span>QA Team Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-5">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic" data-bioName="William"><img src="assets/img/team/WilliamLiang.jpg" class="img-fluid tmpSize" alt="">
+              </div>
+              <div class="member-info">
+                <h4>William Liang</h4>
+                <span>Social Media Member</span>
+                <!--  <div class="social">
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          
+          
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
